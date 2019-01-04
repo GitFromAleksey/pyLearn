bd4f76918d8f27a1bf9125f9f7568f6cbf4950b0
unknown <Aleks>
2019-01-05 01:21:58 +0700

    -

--- FileSorter/file_sorter.py
+++ FileSorter/file_sorter.py
@@ -1,21 +1,32 @@
 import os
 
+sourseFileName = '1.txt'
+
+def findFile(fileName, dir):
+    if os.path.isdir(dir):
+        print(' ', dir,' is dir')
+        for ls in os.listdir(dir):
+            print(ls)
+            if os.path.isfile(ls):
+                print(ls + ' is file')
 
 def main():
     cwd = os.getcwd()
     print(cwd)
 
+    findFile('file', cwd)
+
     # cwd = 'C:\Users\AMD\Desktop\Аня свадьба'.encode()
     # cwd = 'C:\Users\AMD\Desktop\Аня свадьба'
     # dir = 'C:\Users\AMD\Desktop\Аня свадьба'
 
-    for root, dirs, files in os.walk(cwd):
-        print('root: ', root)
-        print('dirs: ', dirs)
-        print('files: ', files)
+    # for root, dirs, files in os.walk(cwd):
+        # print('root: ', root)
+        # print('dirs: ', dirs)
+        # print('files: ', files)
 
-    str = 'Русский шрифт'
-    print('str = ', str)
+    # str = 'Русский шрифт'
+    # print('str = ', str)
 
 if __name__ == '__main__':
     main()
