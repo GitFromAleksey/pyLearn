b09e47e3065d285b3e7f4f6ead13fda20edd1cde
John Doe <johndoe@example.com>
2019-01-07 14:45:02 +0600

    В filesorter сделал обход директорий с помощью os.walk

--- FileSorter/file_sorter.py
+++ FileSorter/file_sorter.py
@@ -1,26 +1,52 @@
 import os
+from fileinput import filename
 
-sourseFileName = '1.txt'
+sourseFileName = 'dir_1.txt'
 
-def findFile(dir):
+def FindFile(fileName,sourseDir):
+    if os.path.isdir(sourseDir):
+        for curDir, dirs, files in os.walk(sourseDir):
+            for file in files:
+                print(fileName, ' - ', file)
+                if filename == file:
+                    print(os.path.join(curDir,file))
 
-    print('findFile(dir): ',dir)
+def PrintAllDirsContent(dirPath):
+    
+    if os.path.isdir(dirPath):
+        for curDir, dirs, files in os.walk(dirPath):
+            for file in files:
+                print('file: ', os.path.join(curDir,file))
+                
+            for dir in dirs:
+                print('directory: ', os.path.join(curDir,dir))
 
-    if os.path.isdir(dir):
-        for ls in os.listdir(dir):
-            if os.path.isdir(ls):
-                print('is dir: ', ls)
-                findFile(os.path.abspath(ls))
+
+def PrintDir(dirPath):
+
+    print('findFile(dir): ',dirPath)
+
+    if os.path.isdir(dirPath):
+        for ls in os.listdir(dirPath):
+#                 PrintDir(os.path.abspath(ls))
             if os.path.isfile(ls):
 #                 print('abspath: ', os.path.abspath(ls))
                 print('is file: ',ls)
-
+            if os.path.isdir(ls):
+                print('is dir: ', ls)
 
 def main():
-    cwd = os.getcwd()
-#     print('current dir: ', cwd)
+    print('os.name: ', os.name)
+    print('os.altsep: ', os.altsep)
 
-    findFile(cwd)
+    cwd = os.getcwd()
+    print('current dir: ', cwd)
+
+#     PrintDir(cwd)
+    path_ = "C:\\Users\\HP-G7000\\eclipse-workspace\\pyLearn"
+
+#     PrintAllDirsContent("C:\\Users\\HP-G7000\\eclipse-workspace\\pyLearn\\FileSorter")
+    FindFile(sourseFileName, cwd)
 
 
 if __name__ == '__main__':
