f51f6b8eed6cecf01e1e48a9647bf55dfaf2be77
Aleksey <a_h.79@mail.ru>
2021-07-19 22:04:33 +0700

    добавлено определение количества камер в системе

--- webcam/webcam.py
+++ webcam/webcam.py
@@ -2,9 +2,20 @@ import cv2.cv2 as cv
 
 print (cv.__version__)
 
+cams_dict = {}
+for i in range(10):
+    cam = cv.VideoCapture(i)
+    if cam.isOpened():
+        cams_dict[i] = cam.getBackendName()
+        print(cam.getBackendName(), i)
+
 capture = cv.VideoCapture(0)
+print(type(capture))
+print('BackendName:', capture.getBackendName())
+print('isOpened:', capture.isOpened())
 
 ret, img = capture.read()
-cv.imshow('', img)
+cv.imshow('Cam name: ' + capture.getBackendName(), img)
+cv.imwrite('img.jpg',img)
 
 capture.release()
