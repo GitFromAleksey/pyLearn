ecad42041dacda079e9a382e691d7dacc823b803
xeon_24 <xeon@xeon.ex>
2024-11-24 19:41:43 +0700

    - написан свой класс для работы с камерой

--- webcam/webcam.py
+++ webcam/webcam.py
@@ -1,10 +1,55 @@
+import time
 import cv2 as cv
 import argparse
 
 print (f'cv2 version: {cv.__version__}')
 
+class Camera:
+
+    def __init__(self) -> None:
+        self.cam = None
+
+    def __del__(self):
+        self.CamClose()
+
+    def CamOpen(self) -> bool:
+        self.cam = cv.VideoCapture(0)
+        return self.cam.isOpened()
+
+    def CamClose(self) -> None:
+        if self.cam:
+            self.cam.release()
+
+    def ShowImage(self) -> bool:
+        cam = self.cam
+        if cam == None:
+            return False
+        if cam.isOpened():
+            ret, img = cam.read()
+            if ret:
+                cv.imshow('Cam name: ' + cam.getBackendName(), img)
+
+    def SaveImage(self):
+        cam = self.cam
+        if cam == None:
+            return False
+        if cam.isOpened():
+            ret, img = cam.read()
+            if ret:
+                cv.imshow('Cam name: ' + cam.getBackendName(), img)
+                cv.imwrite('img.jpg', img)
+
 
 def main():
+
+    cam = Camera()
+    cam.CamOpen()
+    cam.SaveImage()
+    cam.CamClose()
+
+
+    return
+
     cam = cv.VideoCapture(0)
     while True: #cam.isOpened():
         ret, img = cam.read()
@@ -12,6 +57,7 @@ def main():
             cv.imshow('Cam name: ' + cam.getBackendName(), img)
             # cv.waitKey(0) # ожидание нажатия любой клавиши
             # cv.imwrite('img.jpg', img)
+            time.sleep(1)
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
     cam.release()
