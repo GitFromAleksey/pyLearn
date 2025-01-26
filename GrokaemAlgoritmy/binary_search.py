8ffa6b35fc1cec38122b25b7f0ae05274301c601
Aleksey <a_h.79@mail.ru>
2025-01-26 10:50:30 +0700

    - new add

---
+++ GrokaemAlgoritmy/binary_search.py
@@ -0,0 +1,22 @@
+
+
+
+
+
+def binary_search(list, item):
+    low = 0
+    high = len(list)-1
+
+    while low <= high:
+        mid = (low + high)
+        if guess == item:
+
+
+
+def main():
+    lst = [1,3,5,7,9]
+    binary_search(lst, 5)
+
+
+if __name__ == '__main__':
+    main()
